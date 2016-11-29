5f91541ae87cf0eb28625b90d068a9c3a79a3cff
Jason Walker <jwalker14@me.com>
2016-11-29 11:36:00 -0500

    Something

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 var gulp = require('gulp');
 javac = require('gulp-javac');
- 
-gulp.task('example', function() {
+gf_loc = "/usr/local/Cellar/glassfish/4.1.1/libexec/glassfish/domains/domain1/applications/";
+web_root = "restful/";
+
+gulp.task('compile', function() {
   return gulp.src('./src/**/*.java')
-    .pipe(javac('example.jar')
-            .addLibraries('./lib/**/*.jar'))
-            //.addLibraries('./external/**/*.jar'))
-    .pipe(gulp.dest('out'));
+    .pipe(javac.javac({
+      javaVersion: "1.8"
+      }).addLibraries('./lib/**/*.jar'))
+    .pipe(gulp.dest('out/classes'));
+});
+
+gulp.task('web', ['compile'], function(){
+  gulp.src("./web/**/*")
+    .pipe(gulp.dest(gf_loc + web_root))
+});
+
+gulp.task('build', ['web'], function(){
+  gulp.src("./out/classes/*.class")
+    .pipe(gulp.dest(gf_loc + web_root + "WEB-INF/classes/"))
+  return gulp.src("./lib/**/*.jar")
+    .pipe(gulp.dest(gf_loc + web_root + "WEB-INF/lib/"))
 });
 
 
